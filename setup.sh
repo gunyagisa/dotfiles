afdc2ace0e06bbc3859eba2445bb0f62dabbbf47
gunyagisa <49240049+gunyagisa@users.noreply.github.com>
2019-06-04 18:56:11 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
+DOT_FILES=(.zshrc .vimrc)
+do
+    ln -s $HOME/dotfiles/$file $HOME/$file
+done
+
+#dein install
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+cd $HOME/.config
+mkdir vim
+ln -s $HOME/dotfiles/dein.toml $HOME/.config/vim/dein.toml
+ln -s $HOME/dotfiles/dein_lazy.toml $HOME/.config/vim/dein_lazy.toml
